610e3b6e6d54e2e9cb716759256aea66f9ef1906
Vina Fajar Rizqi Edhipeny <vinafjr5@gmail.com>
2019-04-04 14:54:34 +0700

    Display answer for question

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    protected $fillable = ['body', 'user_id'];
+
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
     public function user() {
         return $this->belongsTo(User::class);
     }
---
+++ resources/views/answers/_create.blade.php
@@ -0,0 +1,29 @@
+<div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                <div class="card-title">
+                    <div class="d-flex align-item-center">
+                        <h3>Your Answer</h3>
+                    </div>
+                </div>
+                    <hr>
+                    <form action="{{ route('questions.answers.store', $question->id) }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" rows="7" name="body"></textarea>
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-lg btn-outline-primary">Submit</button>
+                        </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
